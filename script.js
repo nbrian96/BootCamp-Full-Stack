8d7c86e564f642d73492bf13f509f4fc6bd22c98
NISUM <nbrian.avila96@gmail.com>
2021-08-31 19:05:03 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const firstName = 'Brian'
+const firstNameWithUpperCase = firstName.toUpperCase()
+
+console.log(firstNameWithUpperCase)
+
+const list = []
+const anotherList = list.concat(157)
+
+const persona = {
+    name: 'Brian',
+    ig: '@nbrian',
+    age: 25,
+    isDeveloper: true
+}
+
+const sumar = (op1, op2) => {
+    console.log(op1)
+    console.log(op2)
+    return op1 + op2
+}
+
+console.log(sumar(2,5))
